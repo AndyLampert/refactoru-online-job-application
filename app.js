e27bd9a8e4f9e0b413198424dddd397c2b7faaad
AndyLampert <lamperta@gmail.com>
2014-05-15 11:59:47 -0600

    Created a Mongoose model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 app.set('view engine', 'jade');
@@ -7,12 +8,21 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
+var applicant = mongoose.model('Applicant', 
+	// instances of applicant will have a name property that is a string
+	{
+		name: String
+	});
+
+mongoose.connect('mongodb://localhost/Andrew');
+
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
 // displays a list of applicants
 app.get('/applicants', function(req, res){
+	var data = new Data({name: 'testName'});
 	res.render('applicants');
 });
 
@@ -24,7 +34,9 @@ app.get('/success', function(req, res){
 app.get('/applicant', function(req, res){
 	// Here is where you need to get the data
 	// from the post body and store it in the database
+
 	res.redirect('/success');
+
 });
 
 var server = app.listen(8441, function() {
